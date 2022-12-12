9102ced6f21668219fb3a501451717da83bbd1a8
MohanM501 <mohananna501b@gmail.com>
2022-12-12 10:43:57 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-Hi
+const express=require("express")
+require("dotenv").config();
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.get("/name",(req,res)=>{
+    res.send(`The name is ${process.env.name}`)
+})
+app.listen(9000,()=>{
+    console.log("listening on port 9000")
+})
